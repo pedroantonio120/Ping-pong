e190412a3997ebc50e1f33e1df3206627b9e9dfd
pedroantonio120 <gozzi.pedro@escola.pr.gov.br>
2026-03-02 11:57:28 -0300

    update

--- pingpon.js
+++ pingpon.js
@@ -1,43 +1,72 @@
-//Criar Bolinha
-let raio = 7,5;
+// Criar Bolinha
+let raio = 7.5;
 let xBolinha = 100;
 let yBolinha = 200;
 let diametro = raio * 2;
 
-//Velocidade da bolinha 
+// Velocidade da bolinha
 let velocidadeXBolinha = 6;
 let velocidadeYBolinha = 6;
 
-// Minha Raquete 
+// Minha Raquete
 let xRaquete = 5;
-let yRaqeuete = 150;
+let yRaquete = 150;
 
-// Raquete Oponente 
+// Raquete Oponente
 let xRaqueteOponente = 585;
 let yRaqueteOponente = 150;
 
-// Placar 
-let meuPontos = 0;
-let pontosOponentes = 0;
+// Placar
+let meusPontos = 0;
+let pontosOponente = 0;
+
+// dificuldade
+let  dificuldade = "Normal"
+let trilha,ponto,raquetada;
+
+function preload(){
+  trilha = loadSound("trilha.mp3")
+  ponto = loadSound("ponto.mp3")
+  raquetada = loadSound("raquetada.mp3")
+}
 
 function setup(){
   createCanvas(600,400)
-} 
+  trilha.loop()
+}
 
-function draw (){
-  
+function draw(){
+  background(0);
+  movimentoBolinha();
+  raquetes(xRaquete,yRaquete);
+  raquetes(xRaqueteOponente,yRaqueteOponente);
+  npc(dificuldade);
+ 
+  if(keyIsDown(UP_ARROW)){
+    yRaquete -= 10}
+ 
+  if(keyIsDown(DOWN_ARROW)){
+    yRaquete += 10}
+ 
+  if(xBolinha - raio , xRaquete + 10 & yBolinha-raio < yRaquete + 90 & YBolinha + raio > yRaquete){
+    velocidadeXBolinha *= -1;
+    raquetada.play()}
+  if (xBolinha + raio > XRaqueteOponente & yBolinha - raio < yRaqueteOponente + 90 & yBolinha + raio > yRaqueteOponente) {
+    velocidadeXBolinha *= -1
+    raquetada.play()}
 }
 
 function movimentoBolinha(){
-   circle (xBolinha,yBolinha,diametro);
-  
-    xBolinha += velocidadeXBolinha;
-    yBolinha += velocidadeYBolinha;
-    if(xBolinha + raio > width || xBolinha-raio < 0){
-      velocidadeXBolinha *= -1;
-    {;
-     if(yBolinha + raio > height || yBolinha -raio < 0){
-       velocidadeYbolinha *= -1}};
-      
-function raquetes (xraquete,yRaquete){
-  rect(xRaquete,yRaquete,10,90)
+  circle(xBolinha,yBolinha,diametro);
+ 
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+  if(xBolinha + raio > width || xBolinha-raio < 0){
+    velocidadeXBolinha *= -1;
+  }
+  if(yBolinha + raio > height || yBolinha-raio < 0){
+    velocidadeYBolinha *= -1}}
+
+function raquete(xRaquete,yRaquete){
+  rect(xRaquete,yRaquete,10,90 )
+}
